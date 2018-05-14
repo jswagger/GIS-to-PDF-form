10d03aa089930536e330d7a73973e81f14b84c33
Jeremy Swagger <jeremy.swagger@gmail.com>
2018-05-14 13:48:18 -0500

    gets form popultating working

--- gis_to_pdf_form.py
+++ gis_to_pdf_form.py
@@ -33,26 +33,28 @@ def format_data_to_object(layer):
     return json.load(open(path_to_json))
 
 
-def fill_out_form(data, domains, pdftemplate):
-    pdfPath = os.path.dirname("gis_to_pdf_form.py")
-
-    pdf_doc = arcpy.mapping.PDFDocumentCreate(pdfPath)
-    for record in data['features']:
-        record_pdf = "bob"
-        pdf_doc.appendPages(record_pdf)
+def fill_out_form(data, dir_name, template_pdf, pdf_doc):
+    rendered_pdf = template_pdf(data)
+    output_file = os.path.join(dir_name, 'report.pdf')
+    rendered_pdf.write(open(output_file, 'wb'))
+    pdf_doc.appendPages(output_file)
 
 
 def process_each_record(data, domains, pdftemplate):
+    dir_name = os.path.dirname(__file__)
+    pdf_doc = arcpy.mapping.PDFDocumentCreate(dir_name + "\\example_report.pdf")
+    template_pdf_file = os.path.join(dir_name, pdftemplate)
+    template_pdf = PdfJinja(template_pdf_file)
     for record in data:
         record_attributes = record["attributes"]
         adjust_domain_structure(domains, record_attributes)
-        fill_out_form(record_attributes, domains, pdftemplate)
+        fill_out_form(record_attributes, dir_name, template_pdf, pdf_doc)
 
 
 def main():
-    db_connection = "C:\\Users\\jswagger\\Desktop\\Example.gdb"
+    db_connection = "C:\\Users\\jswagger\\Desktop\\GIS-to-PDF-form\\Example.gdb"
     fc_name = "Example_Trees"
-    pdf_template = "C:\\Users\\jswagger\\Desktop\\example_template.pdf"
+    pdf_template = "C:\\Users\\jswagger\\Desktop\\GIS-to-PDF-form\\example_template.pdf"
     layer = get_layer(db_connection, fc_name)
     domains = get_domain_fields(layer)
     object_data = format_data_to_object(layer)
