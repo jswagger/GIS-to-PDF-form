d713f054391cacce857fa15e57385366ca1b1dda
Jeremy Swagger <jeremy.swagger@gmail.com>
2018-05-30 10:55:21 -0500

    continues unit test building

--- test_gis_to_pdf_form.py
+++ test_gis_to_pdf_form.py
@@ -1,8 +1,24 @@
 from unittest import TestCase
 from mock import patch, MagicMock
 import gis_to_pdf_form
+import arcpy
 
 class TestGisToPDF(TestCase):
-
-def test_when_(self):
+  @patch('arcpy.ListFields')
+  def test_getting_domain_fields(self, mock_list):
+    fake_layer = self.get_fake_layer()
+    mock_list.return_value = fake_layer['fields']
+    fake_domains = gis_to_pdf_form.get_domain_fields(fake_layer)
+    expected_domain_list = []
+    self.assertEqual(fake_domains, expected_domain_list)
+    
+   @static
+   def get_fake_layer():
+      layer = MagicMock()
+      layer.fields = [
+        
+      ]
+      return layer
+    
+  
   
